400e5e7f9271cf76d59ddbb104a6ed263ea7efa3
Ivan Bilo <4lish.net@gmail.com>
2022-12-16 18:34:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,11 +14,11 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-dotenv_path = Path('.env')
+dotenv_path = Path(".env")
 load_dotenv(dotenv_path=dotenv_path)
 
-api_id = os.getenv('api_id')
-api_hash = os.getenv('api_hash')
+api_id = os.getenv("api_id")
+api_hash = os.getenv("api_hash")
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
@@ -93,7 +93,6 @@ async def log(client, message):
 
                 # Эмоджи казик
                 if message.dice:
-
                     cwd = os.getcwd()
                     emoji = message.dice.emoji
                     patch = cwd + "/message/" + str(message.chat.id) + "/"
@@ -112,7 +111,6 @@ async def log(client, message):
 
                 # Сообщения Все
                 else:
-
                     cwd = os.getcwd()
                     patch = cwd + "/message/" + str(message.chat.id) + "/"
                     if os.path.exists(patch + "chat.csv"):
@@ -128,6 +126,64 @@ async def log(client, message):
                                 (message.date, message.from_user.username, message.text)
                             )
 
+    if str(messagetype) == "ChatType.GROUP":
+
+        # Создание папки чатов
+
+        parent_dir = "message/"
+        directory = str(str(message.chat.title))
+
+        print(parent_dir + directory)
+
+        if os.path.exists(parent_dir + directory):
+            print("Have Folder")
+        else:
+            path = os.path.join(parent_dir, directory)
+            os.mkdir(path)
+
+        if message.sticker:
+
+            cwd = os.getcwd()
+            emoji = message.sticker.emoji
+            patch = cwd + "/message/" + str(message.chat.title) + "/"
+            if os.path.exists(patch + "chat.csv"):
+                with open(str(patch) + "chat.csv", "a") as file:
+                    writer = csv.writer(file)
+                    writer.writerow((message.date, message.from_user.username, emoji))
+            else:
+                with open(str(patch) + "chat.csv", "w") as file:
+                    writer = csv.writer(file)
+                    writer.writerow((message.date, message.from_user.username, emoji))
+
+        if message.dice:
+            cwd = os.getcwd()
+            emoji = message.dice.emoji
+            patch = cwd + "/message/" + str(message.chat.title) + "/"
+            if os.path.exists(patch + "chat.csv"):
+                with open(str(patch) + "chat.csv", "a") as file:
+                    writer = csv.writer(file)
+                    writer.writerow((message.date, message.from_user.username, emoji))
+            else:
+                with open(str(patch) + "chat.csv", "w") as file:
+                    writer = csv.writer(file)
+                    writer.writerow((message.date, message.from_user.username, emoji))
+
+        # Обычные сообщения
+        else:
+            cwd = os.getcwd()
+            patch = cwd + "/message/" + str(message.chat.title) + "/"
+            if os.path.exists(patch + "chat.csv"):
+                with open(str(patch) + "chat.csv", "a") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(
+                        (message.date, message.from_user.username, message.text)
+                    )
+            else:
+                with open(str(patch) + "chat.csv", "w") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(
+                        (message.date, message.from_user.username, message.text)
+                    )
     else:
         print(message)
 
